f0c7d7adf74692cb671d295de03ba5a11a59d029
Valink16 <valuthyk@gmail.com>
2020-10-05 22:01:56 +0200

    Minor improvement

--- cli_maze.py
+++ cli_maze.py
@@ -1,11 +1,11 @@
 import maze
 
-m = maze.Maze(2, 2)
+m = maze.Maze(50, 50)
 
 while not m.isPerfect():
     m.step()
 
 for row in m.asGrid():
     for c in row:
-        print("[X]" if c else "[ ]", end=" ")
+        print("[X]" if c else "   ", end="")
     print("")
